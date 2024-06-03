d5dd627677184736e5aace0b175d66419f5078c1
Andrii Mytiev <79248454+AndriiMytiev@users.noreply.github.com>
2024-06-03 09:29:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,27 +6,20 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-const corsOptions = {
-  origin:"http://localhost:3000",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 
 app.use(express.json());
+
 app.use("/api", userRouter);
 app.use("/api", musicRouter);
 
 app.get("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS",
-  );
-  res.end("<h1>Home host server</h1>");
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Max-Age", "1800");
+		res.setHeader("Access-Control-Allow-Headers", "content-type");
+		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+  res.end("<h1>Host server</h1>");
 });
 
 app.listen(PORT, () => console.log(`server started on post ${PORT}`));
