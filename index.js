f3cbd6ce16164bd7b2d3626d4a519d03cee1f784
AndriiMytiev <andrijmitev95@gmail.com>
2024-06-03 09:10:20 +0300

    server

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 app.use("/api", userRouter);
 app.use("/api", musicRouter);
 
 app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS",
+  );
   res.end("<h1>Home page</h1>");
 });
 
